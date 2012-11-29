a3c5ad42b21cafcaee9ce4b1a3e52cf39160229c
Kyle Maxwell <kylem@xwell.org>
2012-11-29 21:14:29 +0000

    Moved URL pulling into new function geturl()

--- floorwalker.py
+++ floorwalker.py
@@ -11,22 +11,25 @@ import urllib2
 
 # Planning through commenting!
 
+def geturl(myurl):
+    req = urllib2.Request(myurl)
+    # TODO: use proper loggging, not just stderr
+    try:
+        response = urllib2.urlopen(req)
+    except URLError, e:
+        if hasattr(e,'reason'):
+            sys.stderr.write('urlopen() returned error '+e.reason+'\n')
+        elif hasattr(e,'code'):
+            sys.stderr.write('Server couldn\'t fulfill request: '+e.code+'\n')
+        else:
+            sys.stderr.write('Opened '+testurl+' with response code '+response.getcode()+'\n')
+    return response
+    
 # TODO: instantiated thru cron so check for a lock first
 
 # assuming nothing else is running, get http://pastebin.com/archive
 testurl = "http://pastebin.com/archive"
-
-req = urllib2.Request(testurl)
-# TODO: factor out these urlopens and error checking
-try:
-    response = urllib2.urlopen(req)
-except URLError, e:
-    if hasattr(e,'reason'):
-        sys.stderr.write('urlopen() returned error '+e.reason+'\n')
-    elif hasattr(e,'code'):
-        sys.stderr.write('Server couldn\'t fulfill request: '+e.code+'\n')
-    else:
-        sys.stderr.write('Opened '+testurl+' with response code '+response.getcode()+'\n')
+response = geturl(testurl)
 
 # Generate list of pastes
 soup = BeautifulSoup(response)
@@ -35,6 +38,7 @@ pastes = []
 for td in tabledata:
     try:    
         if td.a['href'].count("/archive/") == 0:
+		# TODO: use regex 
                 pastes.append(td.a['href'])
     except:
         pass
@@ -53,17 +57,9 @@ for paste in pastes:
     # nested try blocks feel bad, man
     if not havepaste:
         pasteurl = 'http://pastebin.com/raw.php?i='+paste
-    	pastereq = urllib2.Request(pasteurl)
+    	pasteresp = geturl(pasteurl)
     	try:
-    	    pasteresp = urllib2.urlopen(pastereq)
-    	except URLError, e:
-    	    if hasattr(e,'reason'):
-    			sys.stderr.write('urlopen() returned error '+e.reason+'\n')
-    	    elif hasattr(e,'code'):
-    			sys.stderr.write('Server couldn\'t fulfill request: '+e.code+'\n')
-    	    else:
-    			sys.stderr.write('Opened '+testurl+' with response code '+response.getcode()+'\n')
-    	try:
+		# TODO: replace with sqlite3
     		pastefile=open('data/'+paste,'w')
     		pastefile.write(pasteresp.read())
     		pastefile.close()
