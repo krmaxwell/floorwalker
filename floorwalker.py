07ae7b98c76301f9e8a1c45cccaf765b2c61e9c4
Kyle Maxwell <kylem@xwell.org>
2012-12-07 04:28:48 +0000

    Added getpaste() and name=main trick

--- floorwalker.py
+++ floorwalker.py
@@ -12,8 +12,23 @@ import time
 import re
 import logging
 import pymongo
+from dateutil.parser import *
+from dateutil.tz import *
+from datetime import *
 
 # Planning through commenting!
+
+def getpaste(pasteID):
+    pastepage = geturl('http://pastebin.com/'+pasteID)
+    # now parse this and return a dict
+    mysoup = BeautifulSoup(pastepage)
+    title = mysoup.title.text
+    author = mysoup.find_all('div', 'paste_box_line2')[0].a.text
+    line = mysoup.find_all('div', 'paste_box_line2')[0].text
+    mydate = parse(line.split(' ')[4] + line.split(' ')[5] +line.split(' ')[6])
+    pastetext = BeautifulSoup(geturl('http://pastebin.com/raw.php?i='+pasteID)).text
+    fullpaste = {'title': title, 'author': author, 'date': mydate, 'paste': pastetext}
+    return fullpaste
 
 def geturl(myurl):
     req = urllib2.Request(myurl)
@@ -29,15 +44,16 @@ def geturl(myurl):
             logging.warning('Opened %s with response code %s',myurl,response.getcode())
     
 # TODO: check for a lock first
+if __name__="__main__":
+    logging.basicConfig(filename='floorwalker.log',format='%(asctime)s %(message)s',datefmt='%Y-%m-%d %H%M.%S',level=logging.DEBUG)
+    archiveurl = "http://pastebin.com/archive"
+    pastere = re.compile("\w")
 
-logging.basicConfig(filename='floorwalker.log',format='%(asctime)s %(message)s',datefmt='%Y-%m-%d %H%M.%S',level=logging.DEBUG)
-archiveurl = "http://pastebin.com/archive"
-pastere = re.compile("\w")
+    # connect to mongodb
+    connection = pymongo.MongoClient
+    db = connection.floorwalker
+    pastes = db.pastes
 
-# connect to mongodb
-
-# Just keep running until somebody tells us to stop
-while True:
     # TODO: handle KeyboardError and kill commands
     logging.info('Getting URL %s', archiveurl)
     response = geturl(archiveurl)
@@ -48,43 +64,40 @@ while True:
     # Be nice if we're going too fast
     if soup.get_text().find('Please slow down'):
         time.sleep(10)
-
+    
     tabledata = soup.find_all('td')
-    # TODO: pastes should be persistent across runs so we don't have to grab it every time
-    pastes = []
     for td in tabledata:
-        try:    
-            if td.a['href'].count("/archive/") == 0:
-                # drop the leading "/"
-                pasteID = td.a['href'][1::]
-                logging.info('Found ref to paste %s', pasteID)
-                pastes.append(pasteID)
-        except:
-            pass
-
-    # Iterate through each listed paste
-    # if we don't already have this one, store it
-    for pasteID in pastes:
-        pastematch = pastere.match(pasteID)
-        paste = []
-        havepaste = # result of checking for key in mongodb
-
-
-        # nested try blocks feel bad, man
-        if not havepaste:
-            time.sleep(2)
-            pasteurl = 'http://pastebin.com/raw.php?i='+pasteID
-            logging.info('Getting paste %s', pasteID)
-            pasteresp = geturl(pasteurl)
-            if hasattr(pasteresp, '__read__'):
-                paste = pasteresp.read()
-                if paste.find('Please slow down'):
-                    time.sleep(10)
-            try:
-            # TODO: replace with sqlite3
-                pastefile=open('data/'+pasteID,'w')
-                pastefile.write(paste)
-                pastefile.close()
-            except:
-                logging.error('ERMAGERD couldn\'t write to file: data/'+pasteID+'\n')
-    time.sleep(60)
+        # TODO: rewrite without the try/except using hasattr()
+        if hasattr(td.a['href'], count) and td.a['href'].count("/archive/") == 0:
+            # drop the leading "/"
+            nextpasteID = td.a['href'][1::]
+            logging.info('Found ref to paste %s', nextpasteID)
+            if not havepaste(nextpasteID):
+                paste = getpaste(nextpasteID)
+    
+        # Iterate through each listed paste
+        # if we don't already have this one, store it
+        for pasteID in pastes:
+            pastematch = pastere.match(pasteID)
+            paste = []
+            havepaste = # result of checking for key in mongodb
+    
+    
+            # nested try blocks feel bad, man
+            if not havepaste:
+                time.sleep(2)
+                pasteurl = 'http://pastebin.com/raw.php?i='+pasteID
+                logging.info('Getting paste %s', pasteID)
+                pasteresp = geturl(pasteurl)
+                if hasattr(pasteresp, '__read__'):
+                    paste = pasteresp.read()
+                    if paste.find('Please slow down'):
+                        time.sleep(10)
+                try:
+                # TODO: replace with sqlite3
+                    pastefile=open('data/'+pasteID,'w')
+                    pastefile.write(paste)
+                    pastefile.close()
+                except:
+                    logging.error('ERMAGERD couldn\'t write to file: data/'+pasteID+'\n')
+        time.sleep(60)
